d67fc57b456de81bd60ccf87f9a1f71961db6f63
wederbn <benjamin.weder@iaas.uni-stuttgart.de>
2020-05-08 14:55:03 +0200

    Skip db initialization if git repo does not contain required folder

--- clone-data-repo.sh
+++ clone-data-repo.sh
@@ -9,7 +9,11 @@ if test -f "$FILE"; then
     chmod 400 /root/.ssh/id_rsa && ssh-keyscan github.com >> /root/.ssh/known_hosts
 # clone repo and copy the file with the sql statements to the folder that is executed on startup of the postgres db
     git clone ${QC_ATLAS_CONTENT_REPOSITORY_URL} ${QC_ATLAS_CONTENT_REPOSITORY_PATH}
-    cp ${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}/* /docker-entrypoint-initdb.d/
+	if [ -d "${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}" ]; then
+        cp ${QC_ATLAS_CONTENT_REPOSITORY_PATH}/${SUBFOLDER_CONTENT_REPO_BACKUP_FILES}/* /docker-entrypoint-initdb.d/
+    else
+	    echo "unable to find specified directory with example data in the repository"
+	fi
 else
     echo "ssh key not present, proceeding with empty db"
 fi
