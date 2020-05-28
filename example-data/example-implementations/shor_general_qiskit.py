709a19ced7421d210a2a41d02216445c1c0b52f5
Marie Salm <github@mielie.de>
2020-05-28 16:14:29 +0200

    Add measurement gates

--- example-data/example-implementations/shor_general_qiskit.py
+++ example-data/example-implementations/shor_general_qiskit.py
@@ -5,5 +5,5 @@ def get_circuit(**kwargs):
     """Get circuit of Shor with input n."""
     n = kwargs["n"]
     shor = Shor(n)
-    shor_circuit = shor.construct_circuit()
+    shor_circuit = shor.construct_circuit(measurement=True)
     return shor_circuit
